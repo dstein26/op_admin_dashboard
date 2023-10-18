3508930db665e53aa02b223759895f943d87a225
D.Stein <rckstrongo@gmail.com>
2023-10-18 08:28:25 -0400

    Worked on layout. Removed placeholder items

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // Side-bar expansion
 const sidebar = document.getElementById("sidebar");
-const collapsedWidth = "3.5em"
+const sidebarCollapsedWidth = "3.5em";
+const itemCollapsedWidth = "1.5em";
+const sidebarTransitionTime = "0.5s";
+
+// Toggle sidebar so it loads compacted
+addEventListener('DOMContentLoaded', (e) => { expandSidebar(); });
 
 function expandSidebar()
 {
@@ -11,17 +16,21 @@ function expandSidebar()
     {
         for(let ii = 0; ii < listItems.length; ii++)
         {
+            listItems[ii].style.transitionDelay = sidebarTransitionTime;
             listItems[ii].style.maxWidth = null;
         }
+        sidebar.style.transitionDelay = "0";
         sidebar.style.maxWidth = null;
     }
     else
     {
         for(let ii = 0; ii < listItems.length; ii++)
         {
-            listItems[ii].style.maxWidth = "1.5em";
+            listItems[ii].style.transitionDelay = null;
+            listItems[ii].style.maxWidth = itemCollapsedWidth;
         }
-        sidebar.style.maxWidth = collapsedWidth;
+        sidebar.style.transitionDelay = null;
+        sidebar.style.maxWidth = sidebarCollapsedWidth;
     }
     
    // sidebar.style.transform = 'scaleX(1)';
