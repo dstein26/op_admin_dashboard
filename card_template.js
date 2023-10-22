385c207786885223f500ff2b35172f1ff57565a3
D.Stein <rckstrongo@gmail.com>
2023-10-21 21:48:22 -0400

    Created template for project cards

---
+++ card_template.js
@@ -0,0 +1,11 @@
+customElements.define("project-card",   
+    class extends HTMLElement{
+        constructor()
+        {
+            super();
+
+            const card_template = document.getElementById("card-template");
+            const card_content = card_template.content;
+            const shadowRoot = this.attachShadow({mode:"open"}).appendChild(card_content.cloneNode(true));
+        }
+});
