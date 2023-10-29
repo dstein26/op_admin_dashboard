0b5667651b020b925aca8f2d88e158dce007899c
D.Stein <rckstrongo@gmail.com>
2023-10-29 08:55:49 -0400

    Added github icon for github link

--- load_projects.js
+++ load_projects.js
@@ -1,14 +1,14 @@
 class Project 
 {
 	title;
-	description;
+	details;
 	issues;
 	ghLink;
 	
-	constructor(t, d, i, l)
+	constructor(t="", d=[], i=[], l="")
 	{
 		this.title = t;
-		this.description = d;
+		this.details = d;
 		this.issues = i;
 		this.ghLink = l;
 	}
@@ -17,8 +17,14 @@ class Project
 
 projects = 
 [
-	new Project("OP_Calculator",
-		"A reverse-polish notation calculator",
+	new Project("Admin Dashboard",
+		["This Project", "For the ODIN Project", "An admin dashboard for an admin to track multiple projects"],
+		["Add More Projects","Add icons to projects", "Credit where icons came from", "Choose a better font",
+		"Sidebar animation is poor", "Sidebar header does not look good when sidebar compressed", "No header"],
+		""
+	),
+	new Project("Calculator",
+		["For the ODIN Project", "A reverse-polish notation calculator"],
 		[
 			"Make into a mobile app",
 			"Numbers can overflow the display area",
@@ -28,7 +34,7 @@ projects =
 		""
 	),
 	new Project("Etch-A-Sketch",
-		"A site where users can create pixel art",
+		["For the ODIN Project", "A site where users can create pixel art"],
 		[
 			"Allow users to save images",
 			"Make the resolution changes smoother",
@@ -42,4 +48,49 @@ addEventListener("load", loadProjects);
 function loadProjects()
 {
 	console.log("Loading")
+
+	const displayArea = document.getElementById("display-area");
+	const projectCard = document.createElement("project-card");
+
+	const img = document.createElement("img");
+	img.setAttribute("src", "Assets/github-mark-white.svg");
+	img.setAttribute("style", "height:90%; margin: 5% 5px 5% 0;");
+
+	const ghLink = document.createElement("a");
+	ghLink.appendChild(img);
+	ghLink.setAttribute("slot", "gh-link");
+	
+
+	const title = document.createElement("h1");
+	title.setAttribute("slot", "project-name");
+
+	const details = document.createElement("p");
+	details.setAttribute("slot", "project-details");
+
+	const issues = document.createElement("p");
+	issues.setAttribute("slot", "project-issues");
+
+	projects.forEach((project) =>
+	{
+		const tempCard = projectCard.cloneNode(true);
+		let tempNode = tempCard.appendChild(title.cloneNode(true));
+		tempNode.innerHTML = project.title;
+
+		project.details.forEach((detail) =>
+		{
+			tempNode = tempCard.appendChild(details.cloneNode(true));
+			tempNode.innerHTML = detail;
+		});
+
+		project.issues.forEach((issue) =>
+		{
+			tempNode = tempCard.appendChild(issues.cloneNode(true));
+			tempNode.innerHTML = issue;
+		});
+
+		tempNode = tempCard.appendChild(ghLink.cloneNode(true));
+		tempNode.setAttribute("href", project.ghLink);
+
+		displayArea.appendChild(tempCard);
+	});
 }
